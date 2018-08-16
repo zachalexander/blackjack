c4fa9463a2eac920768cefb85d3d8b219b7d35ba
zachalexander <zacharya@cep.org>
2018-08-16 19:53:00 -0400

    refactoring functions with john to make more succinct

--- js/script.js
+++ js/script.js
@@ -7,16 +7,36 @@ let playerCards = [];
 let dealerCards = [];
 const suits = ["Hearts", "Spades", "Clubs", "Diamonds"];
 let currentDeck;
-const $hitButton = document.querySelector('.btn__hit-button');
-const $standButton = document.querySelector('.btn__stand-button');
+const $hit = document.querySelector('.btn__hit-button');
+const $stand = document.querySelector('.btn__stand-button');
+
+// ===================
+// EVENT LISTENERS
+// ===================
+
+$hit.addEventListener('click', function () {
+    if (currentDeck === undefined) {
+        currentDeck = firstDeal;
+    }
+    console.log('after hit:', hit(currentDeck, currentDeck.playerCards));
+});
+
+// stand
+// $stand.addEventListener('click', function(){ 
+//     if(currentDeck === undefined){
+//         currentDeck = firstDeal;
+//     };
+//     currentDeck = dealerTurn(currentDeck)
+//     console.log('after player stand:', currentDeck); 
+// });
 
 // ===================
 // GAMEPLAY 
 // ===================
 
 // create deck
-for (let i = 0; i < 4; i++) {
-    for (let j = 2; j < 15; j++) {
+for (let i = 0; i < suits.length; i++) {
+    for (let j = 1; j < 14; j++) {
         let card = {
             suit: suits[i],
             value: j
@@ -25,22 +45,24 @@ for (let i = 0; i < 4; i++) {
     }
 }
 
-// shuffle deck
 deck = shuffleDeck(deck);
 
-// deal deck
-let firstDeal = deal(deck);
+deal(deck);
 
-// hit 
-$hitButton.addEventListener('click', function(){ currentDeck = hit(firstDeal, firstDeal.playerCards) });
+console.group('player deck:');
+console.log(playerCards);
+console.log(sumHand(playerCards));
+console.groupEnd();
 
-// stand
-$standButton.addEventListener('click', function(){ 
-    if(currentDeck === undefined){
-        currentDeck = firstDeal;
-    };
-    dealerTurn(currentDeck) 
-});
+console.group('dealer deck:');
+console.log(dealerCards);
+console.log(sumHand(dealerCards));
+console.groupEnd();
+
+console.group('game deck');
+console.log('deck:', deck);
+console.groupEnd();
+
 
 // ===================
 // FUNCTIONS
@@ -55,62 +77,61 @@ function shuffleDeck(deck) {
     return newDeck;
 }
 
-function deal(deck){
-    let topFourCards = deck.slice(0,4);
-    let remainingDeck = deck.slice(4,52);
-
-    for (let i = 0; i < topFourCards.length; i++){
-        if (i === 0 || i === 2) {
-            playerCards.push(topFourCards[i]);
-        } else {
-            dealerCards.push(topFourCards[i]);
-        }
-    }
-    return {
-        playerCards: playerCards,
-        dealerCards: dealerCards,
-        remainingDeck: remainingDeck
-    }
+function deal(deck) {
+    playerCards.push(dealOneCard(deck));
+    dealerCards.push(dealOneCard(deck));
+    playerCards.push(dealOneCard(deck));
+    dealerCards.push(dealOneCard(deck));
 }
 
-function sumHand(deck) {
-    let deckSum = deck.map(function(element){
-        let faceCardChange = element.value;
-        if (element.value > 10 && element.value < 14) {
-            faceCardChange = 10;
-        }
-        if (element.value === 14){
-            faceCardChange = 11;
-        }
-        return faceCardChange;       
-    });
-    const sumElements = (x, y) => x + y;
-    let handSum = deckSum.reduce(sumElements);
-    return handSum;
+function dealOneCard(deck) {
+    return deck.shift();
 }
 
-function hit(deck, userToAdd) {
+function sumHand(hand) {
+    return hand.reduce(function(accum, current) {
+        let pointValue = current.value;
+        if (pointValue > 10) {
+            pointValue = 10;
+        } else if (pointValue === 1) {
+            // take the original hand remove Ace from it
+            let cardIndex = hand.indexOf(current)
+            let handWithoutAce = hand.splice(cardIndex);
+            // take the sum of that hand
+            let sumWithoutAce = sumHand(handWithoutAce);
+            // determine if there is room for an 11-point card
+            // if so, then make Ace value 11
+            if (sumWithoutAce + 11 < 21) {
+                pointValue = 11;
+            }
+            // if not, leave Ace value as 1
+        }
+        return accum + pointValue;
+    }, 0);
+}
+
+// Need to correct sumHand to allow evaluation of hand if Aces are present
+
+function hit(deck, deckToAddCard) {
     let hitCard = deck.remainingDeck.splice(0, 1);
     let remainingDeck = deck.remainingDeck;
-
-    userToAdd.push(hitCard[0]);
-    deckAfterHit = userToAdd;
+    // console.log('remaining deck:', remainingDeck);
+    deckToAddCard.push(hitCard[0]);
     deck.remainingDeck = remainingDeck;
+    currentDeck = deck;
     return deck;
 }
 
-
 function dealerTurn(deck) {
     let deckToDealer = deck;
-    let playerHandSum = sumHand(deckToDealer.playerCards, playerCards);
-    let dealerHandSum = sumHand(deckToDealer.dealerCards, dealerCards);
+    let playerHandSum = sumHand(deckToDealer.playerCards);
+    let dealerHandSum = sumHand(deckToDealer.dealerCards);
 
-    console.log(deckToDealer);
-    console.log(playerHandSum);
-    console.log(dealerHandSum);
-
-    if (dealerHandSum <= 18) {
-        console.log('dealer hit');
+    if (dealerHandSum <= 17) {
+        currentDeck = hit(deckToDealer, dealerCards);
+        console.log('deck to dealer after hit:', currentDeck);
+    } else {
+        console.log('evaluate hands');
     }
 
 }
